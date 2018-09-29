04b469c2b69a13176bd62be6bc0c42b490037247
vishuvish <vishvishu14@gmail.com>
2018-09-29 10:46:44 +0530

    iterative linear ceil search

---
+++ search/ceil_search_linear.py
@@ -0,0 +1,42 @@
+from random import randint
+
+"""
+Python Code for searching the next greater element than x in array.
+If next greater element is present then return the index of that element.
+If the same element is present in the array then return the index of the searched element.
+Otherwise return None.
+"""
+
+def iterative(array, element):
+    """
+    Perform Linear Search by Iterative Method.
+    :param array: Iterable of elements.
+    :param element: element to be searched.
+    :return: returns value of index of element (if found) else return None.
+    """
+    next_greater = max(array) + 1
+    index = None
+    for i in range(len(array)):
+        if array[i]==element:
+            return i
+        elif array[i]>element:
+            if next_greater!=min(next_greater, array[i]):
+                index = i
+            next_greater = min(next_greater, array[i])
+    return index
+
+
+def main():
+    array = [randint(1,10000) for _ in range(100)]
+    # print(array)
+    element = randint(1,10000)
+    # print(element)
+    result = iterative(array, element)
+    if result is None:
+        print('Iterative Ceil Search : Element not present in array')
+    else:
+        print('Iterative Ceil Search : Element is present at index', result)
+
+
+if __name__ == '__main__':
+    main()
