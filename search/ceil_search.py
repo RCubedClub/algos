08c9ed1924746dbfe8d8e3e238d91f0a6e23fd14
vishuvish <vishvishu14@gmail.com>
2018-10-02 23:13:24 +0530

    Optimized Ceil Search-Recursive

---
+++ search/ceil_search.py
@@ -0,0 +1,53 @@
+from random import randint
+
+
+def recursive(arr, low, high, x):
+
+    """
+    Perform Binary Search by Iterative Method.
+    :param array: Iterable of elements.
+    :param left: start limit of array.
+    :param right: end limit of array.
+    :param element: element to be searched.
+    :param sort: True will sort the array first then search, By default = False.
+    :return: returns value of index of element (if found) else return None.
+    """
+
+    if x <= arr[low]:
+        return low
+
+    if x > arr[high]:
+        return -1
+
+    mid = (low + high) // 2
+
+    if arr[mid] == x:
+        return mid
+
+    elif arr[mid] < x:
+        if mid + 1 <= high and x <= arr[mid + 1]:
+            return mid + 1
+        else:
+            return recursive(arr, mid + 1, high, x)
+
+    else:
+        if mid - 1 >= low and x > arr[mid - 1]:
+            return mid
+        else:
+            return recursive(arr, low, mid - 1, x)
+
+
+def main():
+    array = [randint(1,10000) for _ in range(100)]
+    # print(array)
+    element = randint(1,10000)
+    # print(element)
+    result = recursive(array, 0, 99, element)
+    if result is None:
+        print('Recursive Ceil Search : Element not present in array')
+    else:
+        print('Recursive Ceil Search : Element is present at index', result)
+
+
+if __name__ == '__main__':
+    main()
