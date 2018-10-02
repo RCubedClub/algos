7b41295341b21ed670e804f04e95b42aefd1888f
vishuvish <vishvishu14@gmail.com>
2018-10-02 23:53:09 +0530

    Optimized Ceil Search- Iterative

--- search/ceil_search.py
+++ search/ceil_search.py
@@ -1,5 +1,37 @@
 from random import randint
 
+def iterative(array, element, sort=False):
+    """
+    Perform Ceil Search by Iterative Method.
+    :param array: Iterable of elements.
+    :param element: element to search.
+    :param sort: True will sort the array first then search. By default = False.
+    :return: returns value of index of element (if found) else return None.
+    """
+
+    if sort:
+        array.sort()
+
+    left = 0
+    right = len(array)-1
+
+    if array[left]>=element:
+        return left
+
+    if array[right]<element:
+        return None
+
+    while left<=right:
+        mid = (left + right) // 2
+        if array[mid]==element:
+            return mid
+        elif array[mid]<element<array[mid+1]:
+            return mid+1
+        elif array[mid]>element:
+            right = mid-1
+        else:
+            left = mid+1
+    return None
 
 def recursive(arr, low, high, x, sort=False):
 
@@ -7,7 +39,7 @@ def recursive(arr, low, high, x, sort=False):
         arr.sort()
 
     """
-    Perform Binary Search by Iterative Method.
+    Perform Binary Ceil Search by Recursive Method.
     :param array: Iterable of elements.
     :param left: start limit of array.
     :param right: end limit of array.
@@ -20,7 +52,7 @@ def recursive(arr, low, high, x, sort=False):
         return low
 
     if x > arr[high]:
-        return -1
+        return None
 
     mid = (low + high) // 2
 
@@ -42,15 +74,21 @@ def recursive(arr, low, high, x, sort=False):
 
 def main():
     array = [randint(1,10000) for _ in range(100)]
-    # print(array)
+    print(array)
     element = randint(1,10000)
-    # print(element)
-    result = recursive(array, 0, 99, element)
+    print(element)
+    result = recursive(array, 0, 99, element, True)
     if result is None:
         print('Recursive Ceil Search : Element not present in array')
     else:
         print('Recursive Ceil Search : Element is present at index', result)
 
-
+    result = iterative(array, element, True)
+    if result is None:
+        print('Iterative Ceil Search : Element not present in array')
+    else:
+        print('Iterative Ceil Search : Element is present at index', result)
+    array.sort()
+    print(array)
 if __name__ == '__main__':
     main()
